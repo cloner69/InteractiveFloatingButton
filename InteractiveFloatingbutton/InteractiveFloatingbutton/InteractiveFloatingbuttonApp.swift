d2e8be631c8afde280ded5e254886f3eef1ce30a
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 03:00:33 +0700

    float button

---
+++ InteractiveFloatingbutton/InteractiveFloatingbutton/InteractiveFloatingbuttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveFloatingbuttonApp.swift
+//  InteractiveFloatingbutton
+//
+//  Created by Adrian Suryo Abiyoga on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveFloatingbuttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
